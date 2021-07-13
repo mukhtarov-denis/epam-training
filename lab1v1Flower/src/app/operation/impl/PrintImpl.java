9baad5b2f99d7a4b25626c195b9b5e1f2a7c1cdf
mukhtarov-denis <denis.mukhtarov.314@gmail.com>
2021-07-13 21:37:44 +0300

    пустая строка после вывода на консоль

--- lab1v1Flower/src/app/operation/impl/PrintImpl.java
+++ lab1v1Flower/src/app/operation/impl/PrintImpl.java
@@ -8,6 +8,6 @@ public class PrintImpl implements Print {
     @Override
     public void toConsole(Bouquet bouquet) {
         System.out.printf("Bouquet:\n%s\n", bouquet.toString());
-        System.out.printf("Price: %.2f ð.\n", bouquet.getPrice());
+        System.out.printf("Price: %.2f ð.\n\n", bouquet.getPrice());
     }
 }
