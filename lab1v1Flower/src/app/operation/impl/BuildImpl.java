4c4c159412f456672d6723c738673a3d860818a0
mukhtarov-denis <denis.mukhtarov.314@gmail.com>
2021-08-05 00:08:20 +0300

    рандомное формирование букета

--- lab1v1Flower/src/app/operation/impl/BuildImpl.java
+++ lab1v1Flower/src/app/operation/impl/BuildImpl.java
@@ -1,5 +1,7 @@
 package app.operation.impl;
 
+import java.util.Random;
+
 import app.entity.bouquet.Bouquet;
 import app.entity.bouquet.BouquetFlowers;
 import app.entity.flower.builder.FlowerBuilder;
@@ -8,22 +10,29 @@ import app.factory.FlowerFactory;
 import app.operation.Build;
 
 public class BuildImpl implements Build {
-
+    private Random random = new Random();
+    
     @Override
     public Bouquet buildBouquet() {
         FlowerBuilder flowerBuilder = FlowerFactory.getInstance();
         FlowerDirector flowerDirector = new FlowerDirector();
         Bouquet bouquet = new BouquetFlowers();
-        flowerDirector.buildRose(flowerBuilder);
-        bouquet.addFlower(flowerBuilder.getResult());
-        flowerDirector.buildLily(flowerBuilder);
-        bouquet.addFlower(flowerBuilder.getResult());
-        flowerDirector.buildCalla(flowerBuilder);
-        bouquet.addFlower(flowerBuilder.getResult());
-        flowerDirector.buildCarnation(flowerBuilder);
-        bouquet.addFlower(flowerBuilder.getResult());
-        flowerDirector.buildRose(flowerBuilder);
-        bouquet.addFlower(flowerBuilder.getResult());
+        for (int i = 1; i <= random.nextInt(3) + 1; i++) {
+            flowerDirector.buildRose(flowerBuilder);
+            bouquet.addFlower(flowerBuilder.getResult());
+        }
+        for (int i = 1; i <= random.nextInt(3) + 1; i++) {
+            flowerDirector.buildLily(flowerBuilder);
+            bouquet.addFlower(flowerBuilder.getResult());
+        }
+        for (int i = 1; i <= random.nextInt(3) + 1; i++) {
+            flowerDirector.buildCalla(flowerBuilder);
+            bouquet.addFlower(flowerBuilder.getResult());
+        }
+        for (int i = 1; i <= random.nextInt(3) + 1; i++) {
+            flowerDirector.buildCarnation(flowerBuilder);
+            bouquet.addFlower(flowerBuilder.getResult());
+        }
         return bouquet;
     }
 
