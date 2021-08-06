80ced673585b83f274dc5b41eb2ea981461291dd
mukhtarov-denis <denis.mukhtarov.314@gmail.com>
2021-08-06 16:42:50 +0300

    почвы

---
+++ lab3v1XML/src/main/java/by/epam/training/lab3/v1/entity/Soil.java
@@ -0,0 +1,17 @@
+package by.epam.training.lab3.v1.entity;
+
+public enum Soil {
+    PODZOLIC("Подзолистая"),
+    GROUND("Грунтовая"),
+    SOD_PODZOLIC("Дерново-подзолистая");
+    
+    private String description;
+    
+    private Soil(String description) {
+        this.description = description;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+}
