62aebedc58b95e4382b02e56737b5885ca2b1078
mukhtarov-denis <denis.mukhtarov.314@gmail.com>
2021-08-28 11:52:24 +0300

    database config

---
+++ Course/src/main/java/by/epam/training/course/connection/DBConfig.java
@@ -0,0 +1,61 @@
+package by.epam.training.course.connection;
+
+public class DBConfig {
+    private int initPoolSize;
+    private int maxPoolSize;
+    private int validationTimeout;
+    private String url;
+    private String user;
+    private String password;
+    
+    public DBConfig() {
+    }
+    
+    public int getInitPoolSize() {
+        return initPoolSize;
+    }
+    
+    public void setInitPoolSize(int initPoolSize) {
+        this.initPoolSize = initPoolSize;
+    }
+    
+    public int getMaxPoolSize() {
+        return maxPoolSize;
+    }
+    
+    public void setMaxPoolSize(int maxPoolSize) {
+        this.maxPoolSize = maxPoolSize;
+    }
+    
+    public int getValidationTimeout() {
+        return validationTimeout;
+    }
+    
+    public void setValidationTimeout(int validationTimeout) {
+        this.validationTimeout = validationTimeout;
+    }
+    
+    public String getUrl() {
+        return url;
+    }
+    
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    
+    public String getUser() {
+        return user;
+    }
+    
+    public void setUser(String user) {
+        this.user = user;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }   
+}
