eb8e0c3298a5b703b9cb803c7cf16dcc997b04cf
mukhtarov-denis <denis.mukhtarov.314@gmail.com>
2021-08-30 00:16:17 +0300

    add runner

--- lab3v1XML/src/main/java/by/epam/training/lab3/v1/PlantXMLApp.java
+++ lab3v1XML/src/main/java/by/epam/training/lab3/v1/PlantXMLApp.java
@@ -1,7 +1,17 @@
 package by.epam.training.lab3.v1;
 
+import by.epam.training.lab3.v1.exception.ApplicationException;
+import by.epam.training.lab3.v1.factory.RunnerFactory;
+
 public class PlantXMLApp {
     public static void main(String[] args) {
-        
+        Runner runner = RunnerFactory.getInstance();
+        try {
+            runner.run();
+        } catch (ApplicationException e) {
+            System.out.println("Application error...");
+            System.out.printf("Message: %s\nCause:\n%s\n",e.getMessage(), e.getCause().getMessage());
+        }
+        System.out.println("Done.");
     }
 }
