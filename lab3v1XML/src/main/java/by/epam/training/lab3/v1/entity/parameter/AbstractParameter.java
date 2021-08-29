7e6b54cc2c5f37798ce248d51d6c7374922c39db
mukhtarov-denis <denis.mukhtarov.314@gmail.com>
2021-08-30 00:10:19 +0300

    added parameter note

--- lab3v1XML/src/main/java/by/epam/training/lab3/v1/entity/parameter/AbstractParameter.java
+++ lab3v1XML/src/main/java/by/epam/training/lab3/v1/entity/parameter/AbstractParameter.java
@@ -2,15 +2,21 @@ package by.epam.training.lab3.v1.entity.parameter;
 
 public abstract class AbstractParameter implements Parameter {
     private String name;
-    private String value;
+    protected String note;
+    protected String value;
+        
+    public AbstractParameter(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
     
     @Override
     public String name() {
         return name;
     }
-
+    
     @Override
-    public String value() {
-        return value;
+    public String note() {
+        return "";
     }
 }
--- lab3v1XML/src/main/java/by/epam/training/lab3/v1/entity/parameter/Parameter.java
+++ lab3v1XML/src/main/java/by/epam/training/lab3/v1/entity/parameter/Parameter.java
@@ -2,5 +2,6 @@ package by.epam.training.lab3.v1.entity.parameter;
 
 public interface Parameter {
     String name();
+    String note();
     String value();
 }
