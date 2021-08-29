12c46eb0b5830df7571bd252c18831b106fe9012
mukhtarov-denis <denis.mukhtarov.314@gmail.com>
2021-08-30 00:16:42 +0300

    modified runner

--- lab3v1XML/src/main/java/by/epam/training/lab3/v1/Runner.java
+++ lab3v1XML/src/main/java/by/epam/training/lab3/v1/Runner.java
@@ -1,7 +1,51 @@
 package by.epam.training.lab3.v1;
 
+import java.util.Collections;
+import java.util.List;
+
+import by.epam.training.lab3.v1.comparator.NameComparator;
+import by.epam.training.lab3.v1.comparator.WaterComparator;
+import by.epam.training.lab3.v1.entity.Plant;
+import by.epam.training.lab3.v1.exception.ApplicationException;
+import by.epam.training.lab3.v1.exception.ReaderException;
+import by.epam.training.lab3.v1.exception.WriterException;
+import by.epam.training.lab3.v1.reader.Reader;
+import by.epam.training.lab3.v1.reader.impl.XMLReader;
+import by.epam.training.lab3.v1.writer.Writer;
+import by.epam.training.lab3.v1.writer.impl.XMLWriter;
+import by.epam.training.lab3.v1.xml.XMLErrorHandler;
+import by.epam.training.lab3.v1.xml.XMLValidator;
+
 public class Runner {
-    public void run() {
-        
+    private final String XML_FILE = "plants.xml";
+    
+    public void run() throws ApplicationException {
+        String newFilename = "";
+        XMLValidator xmlValidator = new XMLValidator(XML_FILE, "plants.xsd");
+        XMLErrorHandler errorHandler = new XMLErrorHandler();
+        xmlValidator.setErrorHandler(errorHandler);
+        xmlValidator.validate();
+        if (errorHandler.isError()) {
+            throw new ApplicationException(
+                    String.format("Error validate file \"%s\"\n", XML_FILE),
+                    new Exception(errorHandler.getErrors().toString()));
+        }
+        try {
+            List<Plant> plants = null;
+            Reader xmlReader = new XMLReader(XML_FILE);
+            plants = xmlReader.read();
+            Collections.sort(plants, new NameComparator());
+            newFilename = "plants_sort_by_name.xml";
+            Writer writer = new XMLWriter(newFilename, plants);
+            writer.write();
+            Collections.sort(plants, new WaterComparator());
+            newFilename = "plants_sort_by_water.xml";
+            writer = new XMLWriter(newFilename, plants);
+            writer.write();
+        } catch (ReaderException e) {
+            throw new ApplicationException(String.format("Error read file \"%s\"...", XML_FILE), e);
+        } catch (WriterException e) {
+            throw new ApplicationException(String.format("Error write file \"%s\"...", newFilename), e);
+        }    
     }
 }
