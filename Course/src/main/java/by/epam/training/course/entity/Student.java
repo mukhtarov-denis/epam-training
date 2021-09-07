1a83c127cf71423bc6ed3985fa4241d6432e50be
mukhtarov-denis <denis.mukhtarov.314@gmail.com>
2021-09-07 23:25:29 +0300

    Студенту добавлен список его курсов

--- Course/src/main/java/by/epam/training/course/entity/Student.java
+++ Course/src/main/java/by/epam/training/course/entity/Student.java
@@ -1,11 +1,14 @@
 package by.epam.training.course.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Student extends Person implements Serializable {
     private static final long serialVersionUID = 1L;
     private Integer studentNumber;
     private Integer groupNumber;
+    private List<Course> courses = new ArrayList<>();
     
     public Integer getStudentNumber() {
         return studentNumber;
@@ -21,5 +24,19 @@ public class Student extends Person implements Serializable {
     
     public void setGroupNumber(Integer groupNumber) {
         this.groupNumber = groupNumber;
+    }
+    
+    public List<Course> getCourses() {
+        return courses;
+    }
+    
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        Student other = (Student) obj;
+        return getId() == other.getId();
     }
 }
