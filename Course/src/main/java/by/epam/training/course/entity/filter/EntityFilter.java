1f3c0d7875e44ee1b843c718f3832f02be4b3454
mukhtarov-denis <denis.mukhtarov.314@gmail.com>
2021-09-07 23:34:22 +0300

    changed filter name

--- Course/src/main/java/by/epam/training/course/entity/filter/EntityFilter.java
+++ Course/src/main/java/by/epam/training/course/entity/filter/EntityFilter.java
@@ -2,6 +2,6 @@ package by.epam.training.course.entity.filter;
 
 import by.epam.training.course.entity.Entity;
 
-public interface Filter<T extends Entity> {
+public interface EntityFilter<T extends Entity> {
     boolean check(T entity);
 }
