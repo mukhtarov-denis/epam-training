4e2fb28d6597eb3df9f90ba36f33ca588447af5d
mukhtarov-denis <denis.mukhtarov.314@gmail.com>
2021-09-16 22:06:30 +0300

    В сущность Teacher добавлен Course, композиция

--- Course/src/main/java/by/epam/training/course/entity/Teacher.java
+++ Course/src/main/java/by/epam/training/course/entity/Teacher.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 public class Teacher extends Person implements Serializable {
     private static final long serialVersionUID = 1L;
     private String post;
+    private Course course;
     
     public String getPost() {
         return post;
@@ -12,5 +13,13 @@ public class Teacher extends Person implements Serializable {
     
     public void setPost(String post) {
         this.post = post;
+    }
+    
+    public Course getCourse() {
+        return course;
+    }
+    
+    public void setCourse(Course course) {
+        this.course = course;
     }
 }
