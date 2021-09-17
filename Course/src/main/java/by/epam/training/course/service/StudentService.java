c2a28634643ac441f8666249527db4bdc26b9eed
mukhtarov-denis <denis.mukhtarov.314@gmail.com>
2021-09-17 20:59:46 +0300

    Modified. Delete method findByTeacher

--- Course/src/main/java/by/epam/training/course/service/StudentService.java
+++ Course/src/main/java/by/epam/training/course/service/StudentService.java
@@ -1,11 +1,6 @@
 package by.epam.training.course.service;
 
-import java.util.List;
-
 import by.epam.training.course.entity.Student;
-import by.epam.training.course.entity.Teacher;
-import by.epam.training.course.exception.ServiceException;
 
 public interface StudentService extends BaseService<Student> {
-    List<Student> findByTeacher(Teacher teacher) throws ServiceException;
 }
